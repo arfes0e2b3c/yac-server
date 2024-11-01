bb038228b18ae9f1ddc16535d8343f3c37a27327
arfes0e2b3c <webyata@gmail.com>
2024-11-01 11:55:48 +0900

    fix: usersのpostのパスを修正

--- openapi/user.ts
+++ openapi/user.ts
@@ -87,7 +87,7 @@ export const fetchMeRoute = createRoute({
 })
 
 export const createUserRoute = createRoute({
-	path: '/',
+	path: '/users',
 	method: 'post',
 	description: 'ユーザーを新規追加する',
 	request: {
